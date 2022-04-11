1af6218b52e0dd99b0ebb3e24a63f987bdc7f2ec
Levis Odhiambo <48948403+dalyzhee@users.noreply.github.com>
2022-04-11 10:42:58 +0300

    second commit

--- Laptopgarage/shop/urls.py
+++ Laptopgarage/shop/urls.py
@@ -5,5 +5,7 @@ app_name = "shop"
 urlpatterns = [
     path('', views.index, name="index"),
     path('product/', views.product, name='product'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
     path('service/', views.service, name='service'),
+    path('<category>/', views.category, name='category'),
 ]
--- Laptopgarage/shop/views.py
+++ Laptopgarage/shop/views.py
@@ -19,9 +19,26 @@ def product(request):
     }
     return render(request, 'product.html', context)
 
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    categorys = Category.objects.all()
+    context = {
+        'product': product,
+        'categorys': categorys
+    }
+    return render(request, 'product_detail.html', context)
+
 def service(request):
     services = Service.objects.all()
     context = {
         'services': services
     }
     return render(request, 'service.html', context)
+
+def category(request, category):
+    products = Product.objects.filter(categories__name__contains = category)
+    context = {
+        'products': products,
+        'category': category
+    }
+    return render(request, 'category.html', context)
